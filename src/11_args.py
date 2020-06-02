ecbc5fc826823fd4ffee07afd4459be147140b53
Joey Nevarez <josephnevarez12@gmail.com>
2020-06-02 14:44:41 -0700

    finishes files 11

--- src/11_args.py
+++ src/11_args.py
@@ -27,7 +27,7 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-########################################################################print(f2(a))    # Should print 22
+print(f2(sum(a)))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -51,23 +51,22 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
-def f4(a, b):
-    print(f"key: a, value: {a}, key: b, value: {b}")
+def f4(**kwargs):
+    for key, value in kwargs.items():
+        print(f"key: {key}, value: {value}")
 
 # Should print
 # key: a, value: 12
 # key: b, value: 30
 f4(a=12, b=30)
 
-####################################### SHOULD THIS BE A SEPARATE FUNCTION?
+
 # Should print
 # key: city, value: Berkeley
 # key: population, value: 121240
 # key: founded, value: "March 23, 1868"
-def new_f4(city, population, founded):
-    print(f"key: city, value: {city}, key: population, value: {population}, key: founded, value: {founded}")
 
-new_f4(city="Berkeley", population=121240, founded="March 23, 1868")
+f4(city="Berkeley", population=121240, founded="March 23, 1868")
 
 d = {
     "monster": "goblin",
@@ -75,4 +74,4 @@ d = {
 }
 
 # How do you have to modify the f4 call below to make this work?
-new_f4(d)
+f4(**d)
