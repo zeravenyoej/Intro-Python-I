fc873edbc3bce91e60f1fcdbdd1cd8146c91f3b9
Joey Nevarez <josephnevarez12@gmail.com>
2020-06-02 14:05:24 -0700

    finishes file 08

--- src/11_args.py
+++ src/11_args.py
@@ -5,6 +5,8 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(x, y):
+    return x + y
 
 print(f1(1, 2))
 
@@ -13,6 +15,9 @@ print(f1(1, 2))
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+def f2(*nums):
+    return sum(nums)
+
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -22,7 +27,7 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
+########################################################################print(f2(a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -30,6 +35,8 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(x, y = 1):
+    return x + y
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -44,17 +51,23 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(a, b):
+    print(f"key: a, value: {a}, key: b, value: {b}")
 
 # Should print
 # key: a, value: 12
 # key: b, value: 30
 f4(a=12, b=30)
 
+####################################### SHOULD THIS BE A SEPARATE FUNCTION?
 # Should print
 # key: city, value: Berkeley
 # key: population, value: 121240
 # key: founded, value: "March 23, 1868"
-f4(city="Berkeley", population=121240, founded="March 23, 1868")
+def new_f4(city, population, founded):
+    print(f"key: city, value: {city}, key: population, value: {population}, key: founded, value: {founded}")
+
+new_f4(city="Berkeley", population=121240, founded="March 23, 1868")
 
 d = {
     "monster": "goblin",
@@ -62,4 +75,4 @@ d = {
 }
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+new_f4(d)
